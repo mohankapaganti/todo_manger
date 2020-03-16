7fc7314398eb7accd25f829b0fe5e0073032cce3
mohankapaganti <mohan.kapaganti@gmail.com>
2020-03-17 01:22:58 +0530

    Added ability to create a todo and adds to DB using GET request

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -5,4 +5,8 @@ end
 def show
 render plain: Todo.find(params[:id]).to_pleasant_string
 end
+def create
+new_todo=Todo.create!(todo_text: params[:todo_text],due_date: Date.parse(params[:due_date]),completed: false)
+render plain: "The created todo id id #{new_todo.id}"
+end
 end
