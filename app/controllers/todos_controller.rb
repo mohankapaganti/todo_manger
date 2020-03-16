ee3dd4e7abf2a71224e5dacee6427b7de6cae02a
mohankapaganti <mohan.kapaganti@gmail.com>
2020-03-17 01:09:40 +0530

    Render a todo by todos/:id

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,4 +2,7 @@ class TodosController < ApplicationController
 def index
   render plain: Todo.all.map{|todo| todo.to_pleasant_string}.join("\n")
 end
+def show
+render plain: Todo.find(params[:id]).to_pleasant_string
+end
 end
