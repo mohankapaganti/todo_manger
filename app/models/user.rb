48141f3768a24ecbad158584000d1e7788eaac38
mohankapaganti <mohan.kapaganti@gmail.com>
2020-05-01 15:32:45 +0530

    checking credentials by sending user details by post request

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   def to_pleasant_string
     "#{id}. Name:#{name} Email:#{email} Password:#{password}"
   end
+  def self.check_crendentials(email, password)
+    where("email=? and password=?", email, password).exists?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'users/index'
  resources :todos,:users
    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+   post "users/:id/login", to: "users#login"
 end
