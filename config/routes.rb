ea465baadc7fa258895ab776bc6d864d32d74d25
mohankapaganti <mohan.kapaganti@gmail.com>
2020-05-01 16:31:30 +0530

    Generated home controller using root page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
  resources :todos,:users
    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    post "users/:id/login", to: "users#login"
+   root to: "home#index"
 end
