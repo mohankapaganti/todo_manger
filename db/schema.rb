916987a70017cea5cf1962928f9316931f3aa371
mohankapaganti <mohan.kapaganti@gmail.com>
2020-05-01 14:01:48 +0530

    users table created by rails generated model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_16_190858) do
+ActiveRecord::Schema.define(version: 2020_05_01_082919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 2020_03_16_190858) do
     t.text "todo_text"
     t.date "due_date"
     t.boolean "completed"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
